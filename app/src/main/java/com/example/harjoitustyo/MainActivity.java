528ac10cd17d3fd3e3ccde60fb83a7cb8621ba6b
JR <jesse.ronkko@eng.tamk.fi>
2019-04-04 13:11:28 +0300

    Set background now works

--- app/src/main/java/com/example/harjoitustyo/MainActivity.java
+++ app/src/main/java/com/example/harjoitustyo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.harjoitustyo;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,25 +12,56 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String currentBackgroundColor = "#6495ED";
+
+    //init UI components
     ImageButton btn_setBackground_blue, btn_setBackground_red;
     ConstraintLayout cl_mainLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // find views
         cl_mainLayout=findViewById(R.id.constraintLayout_main);
-
         btn_setBackground_blue = findViewById(R.id.imageButton_blue_background);
         btn_setBackground_red = findViewById(R.id.imageButton_red_background);
+
+        //init background color to blue
+        cl_mainLayout.setBackgroundColor(Color.parseColor(currentBackgroundColor));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+
+        //init setBackground buttons color
+        btn_setBackground_red.setColorFilter(Color.parseColor("#F08080"));
+        btn_setBackground_blue.setColorFilter(Color.parseColor("#6495ED"));
+
     }
 
+    // function to change background color to blue
     public void setBackground_blue (View V) {
         cl_mainLayout.setBackgroundColor(Color.parseColor("#6495ED"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        currentBackgroundColor = "#6495ED";
     }
 
+    // function to change background color to red
     public void setBackground_red (View V) {
         cl_mainLayout.setBackgroundColor(Color.parseColor("#F08080"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        currentBackgroundColor = "#F08080";
+    }
+
+    // intent to open WeatherActivity
+    public void WeatherActivity(View view) {
+        Intent intent = new Intent(this, WeatherActivity.class);
+
+        //sent currentBackgroundColor as extra
+        intent.putExtra("current_background_color", currentBackgroundColor);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/harjoitustyo/WeatherActivity.java
+++ app/src/main/java/com/example/harjoitustyo/WeatherActivity.java
@@ -1,13 +1,30 @@
 package com.example.harjoitustyo;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    ConstraintLayout cl_weather_layout;
+    String currentBackgroundColor = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        cl_weather_layout=findViewById(R.id.constraintLayout_weather);
+
+        //Get intent extra
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            currentBackgroundColor = bundle.getString("current_background_color");
+        }
+
+        //Set background to previously found color code stored in extra
+        cl_weather_layout.setBackgroundColor(Color.parseColor(currentBackgroundColor));
+
     }
 }
