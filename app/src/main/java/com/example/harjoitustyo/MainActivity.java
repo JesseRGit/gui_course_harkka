59b2bb9c8cffbd4f2e54904b5f0d024d2d4990c0
JesseRGit <jesseronkko@hotmail.com>
2019-04-05 14:58:30 +0300

    Added more background color options

--- app/src/main/java/com/example/harjoitustyo/MainActivity.java
+++ app/src/main/java/com/example/harjoitustyo/MainActivity.java
@@ -12,10 +12,10 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String currentBackgroundColor = "#6495ED";
+    public static String currentBackgroundColor = "#F5F5F5";
 
     //init UI components
-    ImageButton btn_setBackground_blue, btn_setBackground_red;
+    ImageButton btn_setBackground_blue, btn_setBackground_red, btn_setBackground_yellow, btn_setBackground_green, btn_setBackground_white;
     ConstraintLayout cl_mainLayout;
 
     @Override
@@ -27,31 +27,77 @@ public class MainActivity extends AppCompatActivity {
         cl_mainLayout=findViewById(R.id.constraintLayout_main);
         btn_setBackground_blue = findViewById(R.id.imageButton_blue_background);
         btn_setBackground_red = findViewById(R.id.imageButton_red_background);
+        btn_setBackground_green = findViewById(R.id.imageButton_green_background);
+        btn_setBackground_yellow = findViewById(R.id.imageButton_yellow_background);
+        btn_setBackground_white = findViewById(R.id.imageButton_white_background);
 
-        //init background color to blue
+
+        //init background color to white
         cl_mainLayout.setBackgroundColor(Color.parseColor(currentBackgroundColor));
-        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
         btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#D3D3D3"));
 
         //init setBackground buttons color
-        btn_setBackground_red.setColorFilter(Color.parseColor("#F08080"));
+        btn_setBackground_white.setColorFilter(Color.parseColor("#F5F5F5"));
+        btn_setBackground_yellow.setColorFilter(Color.parseColor("#F0E68C"));
         btn_setBackground_blue.setColorFilter(Color.parseColor("#6495ED"));
-
+        btn_setBackground_red.setColorFilter(Color.parseColor("#F08080"));
+        btn_setBackground_green.setColorFilter(Color.parseColor("#90EE90"));
     }
 
     // function to change background color to blue
     public void setBackground_blue (View V) {
         cl_mainLayout.setBackgroundColor(Color.parseColor("#6495ED"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#D3D3D3"));
         btn_setBackground_blue.setBackgroundColor(Color.parseColor("#A9A9A9"));
         btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#D3D3D3"));
         currentBackgroundColor = "#6495ED";
     }
+
+    public void setBackground_white (View V) {
+        cl_mainLayout.setBackgroundColor(Color.parseColor("#F5F5F5"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        currentBackgroundColor = "#F5F5F5";
+    }
+
+    public void setBackground_yellow (View V) {
+        cl_mainLayout.setBackgroundColor(Color.parseColor("#F0E68C"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        currentBackgroundColor = "#F0E68C";
+    }
+
+    public void setBackground_green (View V) {
+        cl_mainLayout.setBackgroundColor(Color.parseColor("#90EE90"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_red.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        currentBackgroundColor = "#90EE90";
+    }
+
 
     // function to change background color to red
     public void setBackground_red (View V) {
         cl_mainLayout.setBackgroundColor(Color.parseColor("#F08080"));
+        btn_setBackground_white.setBackgroundColor(Color.parseColor("#D3D3D3"));
+        btn_setBackground_yellow.setBackgroundColor(Color.parseColor("#D3D3D3"));
         btn_setBackground_blue.setBackgroundColor(Color.parseColor("#D3D3D3"));
         btn_setBackground_red.setBackgroundColor(Color.parseColor("#A9A9A9"));
+        btn_setBackground_green.setBackgroundColor(Color.parseColor("#D3D3D3"));
         currentBackgroundColor = "#F08080";
     }
 
