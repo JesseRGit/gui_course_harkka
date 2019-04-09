c3fe25358229cb33c864195bc91367ba5fa92f2e
JR <jesse.ronkko@eng.tamk.fi>
2019-04-09 19:08:58 +0300

    Added handling empty input

--- app/src/main/java/com/example/harjoitustyo/MainActivity.java
+++ app/src/main/java/com/example/harjoitustyo/MainActivity.java
@@ -172,72 +172,75 @@ public class MainActivity extends AppCompatActivity {
 
         // if there's no user input use tampere
         if (city.length() <= 0) {
-            city = "tampere";
-        }
+            //city = "tampere";
+            tv_msg2.setTextColor(Color.parseColor("#DC143C"));
+            tv_msg2.setText("You must type something.");
+        } else {
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        String url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID=6c433438776b5be4ac86001dc88de74d";
+            // Instantiate the RequestQueue.
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=6c433438776b5be4ac86001dc88de74d";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // Process the JSON
-                        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Process the JSON
+                            try {
 
-                            String cityName = response.getString("name");
+                                String cityName = response.getString("name");
 
-                            JSONObject sys = response.getJSONObject("sys");
-                            String countryName = sys.getString("country");
+                                JSONObject sys = response.getJSONObject("sys");
+                                String countryName = sys.getString("country");
 
-                            JSONArray weatherArray = response.getJSONArray("weather");
-                            JSONObject weatherItem = weatherArray.getJSONObject(0);
-                            String descriptionText = weatherItem.getString("description");
+                                JSONArray weatherArray = response.getJSONArray("weather");
+                                JSONObject weatherItem = weatherArray.getJSONObject(0);
+                                String descriptionText = weatherItem.getString("description");
 
-                            JSONObject main = response.getJSONObject("main");
-                            String temperatureValue = main.getString("temp");
+                                JSONObject main = response.getJSONObject("main");
+                                String temperatureValue = main.getString("temp");
 
-                            city = cityName;
-                            country = countryName;
-                            temperature = temperatureValue;
-                            description = descriptionText;
+                                city = cityName;
+                                country = countryName;
+                                temperature = temperatureValue;
+                                description = descriptionText;
 
-                            Intent intent = new Intent(getApplicationContext(), WeatherActivity.class);
+                                Intent intent = new Intent(getApplicationContext(), WeatherActivity.class);
 
-                            // Sent currentBackgroundColor, city, country code, temperature and description as extra
-                            intent.putExtra("current_background_color", currentBackgroundColor);
-                            intent.putExtra("city", city);
-                            intent.putExtra("country", country);
-                            intent.putExtra("temperature", temperature);
-                            intent.putExtra("description", description);
-                            startActivity(intent);
-                        } catch (JSONException e) {
+                                // Sent currentBackgroundColor, city, country code, temperature and description as extra
+                                intent.putExtra("current_background_color", currentBackgroundColor);
+                                intent.putExtra("city", city);
+                                intent.putExtra("country", country);
+                                intent.putExtra("temperature", temperature);
+                                intent.putExtra("description", description);
+                                startActivity(intent);
+                            } catch (JSONException e) {
+                                tv_msg2.setTextColor(Color.parseColor("#DC143C"));
+                                tv_msg2.setText("Can't connect to OpenWeatherApi, try again!");
+                                Toast.makeText(getApplicationContext(), "JSONException occurred", Toast.LENGTH_SHORT).show();
+                                e.printStackTrace();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Do something when error occurred
                             tv_msg2.setTextColor(Color.parseColor("#DC143C"));
-                            tv_msg2.setText("Can't connect to OpenWeatherApi, try again!");
-                            Toast.makeText(getApplicationContext(), "JSONException occurred", Toast.LENGTH_SHORT).show();
-                            e.printStackTrace();
+                            tv_msg2.setText("No city found by that name.");
+
+                            if (et_searchField.requestFocus()) {
+                                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                            }
                         }
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Do something when error occurred
-                        tv_msg2.setTextColor(Color.parseColor("#DC143C"));
-                        tv_msg2.setText("No city found by that name.");
+            );
 
-                        if(et_searchField.requestFocus()) {
-                            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-                        }
-                    }
-                }
-        );
-
-        // Add JsonObjectRequest to the RequestQueue
-        queue.add(jsonObjectRequest);
+            // Add JsonObjectRequest to the RequestQueue
+            queue.add(jsonObjectRequest);
+        }
     }
 }
